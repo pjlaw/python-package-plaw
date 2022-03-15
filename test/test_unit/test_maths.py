5c312f7317c1e552ac87186fba88585d1b192898
pjlaw <phililaw.spam@gmail.com>
2022-03-15 13:37:03 +0000

    reformatting

--- test/test_unit/test_maths.py
+++ test/test_unit/test_maths.py
@@ -1,10 +1,11 @@
 from python_package_plaw.maths import add_numbers, subtract_numbers
 
+
 def test_add_numbers():
     assert add_numbers(1, 2) == 3
     assert add_numbers(10, 100) == 110
 
+
 def test_subtract_numbers():
     assert subtract_numbers(1, 2) == -1
     assert subtract_numbers(10, 2) == 8
-    
