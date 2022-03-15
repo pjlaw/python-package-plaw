e167550f03855a36a577be0af1bea8179af8e884
pjlaw <phililaw.spam@gmail.com>
2022-03-15 14:10:00 +0000

    precommit checks

--- python_package_plaw/maths.py
+++ python_package_plaw/maths.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-    return a + b
+from typing import Union
+
+
+def add_numbers(a: Union[int, float], b: Union[int, float]) -> int:
+    return int(a + b)
 
 
 def subtract_numbers(a, b):
